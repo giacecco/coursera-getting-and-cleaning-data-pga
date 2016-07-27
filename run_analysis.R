9b39a23139f670e4d0d57e08058144e65da856c7
Gianfranco Cecconi <giacecco@dico.im>
2016-07-27 11:25:09 +0100

    Added the original source data from UCI

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+# Note that the original source data from the UC Irvine Machine Learning 
+# repository was extracted and made available in the "UCI-HAR-Dataset" folder.
+
+
+
+
+
+# as from specifications...
+write.table(d, row.name = FALSE)
